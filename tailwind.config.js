317bc5034489541d44c50e85ba02f03574bc2184
Tony <totran0331@gmail.com>
2022-10-31 02:04:14 -0400

    color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       extend: {
          colors: {
             background: "#121212",
-            "light-background": "#1c1c1c",
+            "light-background": "#1F1F1F",
             "light-background-border": "#373637",
             "label-text": "#CCCCCC",
             "input-text": "#9B9B9B",
