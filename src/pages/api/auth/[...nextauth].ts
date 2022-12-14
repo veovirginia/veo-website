ccb3e1ebdfac61b5fbbcdc272a76e364be29e97f
T <totran0331@gmail.com>
2022-12-14 16:58:05 -0500

    formatting fixes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from '../../../../lib/prismadb'
+import prisma from "../../../../lib/prismadb"
 import NextAuth from "next-auth/next"
 import CredentialsProvider from "next-auth/providers/credentials"
 
@@ -7,12 +7,16 @@ export default NextAuth({
    adapter: PrismaAdapter(prisma),
    providers: [
       CredentialsProvider({
-         name: 'Credentials',
+         name: "Credentials",
          credentials: {
-            username: { label: "Username", type: "text", placeholder: "jsmith" },
-            password: {  label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
+            username: {
+               label: "Username",
+               type: "text",
+               placeholder: "jsmith",
+            },
+            password: { label: "Password", type: "password" },
+         },
+         async authorize(credentials, req) {
             // You need to provide your own logic here that takes the credentials
             // submitted and returns either a object representing a user or value
             // that is false/null if the credentials are invalid.
@@ -20,22 +24,22 @@ export default NextAuth({
             // You can also use the `req` object to obtain additional parameters
             // (i.e., the request IP address)
             const res = await fetch("/your/endpoint", {
-              method: 'POST',
-              body: JSON.stringify(credentials),
-              headers: { "Content-Type": "application/json" }
+               method: "POST",
+               body: JSON.stringify(credentials),
+               headers: { "Content-Type": "application/json" },
             })
             const user = await res.json()
-      
+
             // If no error and we have user data, return it
             if (res.ok && user) {
-              return user
+               return user
             }
             // Return null if user data could not be retrieved
             return null
-          }      
-      })
+         },
+      }),
    ],
-   secret: NEXTAUTH_SECRET,
+   secret: process.env.NEXTAUTH_SECRET,
    pages: {
       signIn: "/login",
    },
