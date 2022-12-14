0cd96d18e8f6457484830216403c2878a682a15a
Tony <totran0331@gmail.com>
2022-12-14 15:55:03 -0500

    added prisma

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,48 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import NextAuth from "next-auth/next"
-import GoogleProvider from "next-auth/providers/google"
+// import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET
 
 export default NextAuth({
+   adapter: PrismaAdapter(prisma),
    providers: [
-      GoogleProvider({
-         name: "Google",
-         clientId: GOOGLE_CLIENT_ID,
-         clientSecret: GOOGLE_CLIENT_SECRET,
-      }),
+      CredentialsProvider({
+         name: 'Credentials',
+         credentials: {
+            username: { label: "Username", type: "text", placeholder: "jsmith" },
+            password: {  label: "Password", type: "password" }
+          },
+          async authorize(credentials, req) {
+            // You need to provide your own logic here that takes the credentials
+            // submitted and returns either a object representing a user or value
+            // that is false/null if the credentials are invalid.
+            // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+            // You can also use the `req` object to obtain additional parameters
+            // (i.e., the request IP address)
+            const res = await fetch("/your/endpoint", {
+              method: 'POST',
+              body: JSON.stringify(credentials),
+              headers: { "Content-Type": "application/json" }
+            })
+            const user = await res.json()
+      
+            // If no error and we have user data, return it
+            if (res.ok && user) {
+              return user
+            }
+            // Return null if user data could not be retrieved
+            return null
+          }      
+      })
+      // GoogleProvider({
+      //    name: "Google",
+      //    clientId: GOOGLE_CLIENT_ID,
+      //    clientSecret: GOOGLE_CLIENT_SECRET,
+      // }),
    ],
    secret: NEXTAUTH_SECRET,
    pages: {
