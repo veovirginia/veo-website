00eb633dfceeb4a01eb7dd146bdef19103575cba
T <totran0331@gmail.com>
2022-12-16 16:47:47 -0500

    added registration functions

---
+++ src/helpers/auth/index.ts
@@ -0,0 +1,4 @@
+export { default as hash } from "./hash"
+export { default as validatePassword } from "./validatePassword"
+export { default as validateEmail } from "./validateEmail"
+export { default as validateName } from "./validateName"
