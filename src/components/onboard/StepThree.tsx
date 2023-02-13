5a7e15836d6611fa6fada00d9d719e4c08d7ca28
Tony <totran0331@gmail.com>
2023-02-13 02:55:11 -0500

    Added info from previous onboard steps to cal form

--- src/components/onboard/StepThree.tsx
+++ src/components/onboard/StepThree.tsx
@@ -5,14 +5,17 @@ import Cal, { getCalApi } from "@calcom/embed-react"
 import { OnboardContext } from "../../context/onboardContext"
 import axios from "axios"
 import Alert from "../Alert"
-import Router from "next/router"
+import { useRouter } from "next/router"
 import { motion } from "framer-motion"
+import { useSession } from "next-auth/react"
 
 export default function StepThree() {
    const formContext = useContext(OnboardContext)
    const [isScheduled, setScheduled] = useState(false)
    const [isVisible, setVisible] = useState(true)
    const [message, setMessage] = useState("")
+   const router = useRouter()
+   const { data: session } = useSession()
    useEffect(() => {
       ;(async function () {
          const cal = await getCalApi()
@@ -46,13 +49,11 @@ export default function StepThree() {
 
    const submitHandler = async () => {
       try {
-         const { data } = await axios("/api/user", {
+         const res = await axios("/api/user", {
             method: "put",
-            data: formContext?.formValues?.formValues.info,
+            data: formContext?.formValues.info,
          })
-         if (data.success) {
-            Router.push("/pending")
-         }
+         if (res.status === 200) router.push("/pending")
       } catch (error: any) {
          setMessage(
             "Unable to update user information. Please try again later."
@@ -87,6 +88,11 @@ export default function StepThree() {
                <Cal
                   calLink="entrepreneurship/onboard"
                   style={{ width: "100%", height: "100%", overflow: "auto" }}
+                  config={{
+                     name: formContext?.formValues.info.name,
+                     email: session?.user?.email,
+                     notes: `Meeting with ${formContext?.formValues.meeting.member[0]}`,
+                  }}
                />
             </div>
             <div className="max-w-2xl mx-auto w-full flex justify-between items-center pb-4">
@@ -100,7 +106,7 @@ export default function StepThree() {
                <button
                   type="button"
                   disabled={!isScheduled}
-                  onClick={() => submitHandler()}
+                  onClick={() => submitHandler}
                   className={cn("rounded border px-8 py-2", {
                      "bg-zinc-50 text-neutral-900 border-zinc-50": isScheduled,
                      "bg-noir-800/30 text-noir-600 border-noir-800 cursor-not-allowed":
