4ad9ec2f66affafe460eb6f1694da4b86025fdbc
Tony <totran0331@gmail.com>
2023-08-31 19:21:29 -0400

    restored put file

---
+++ src/pages/api/user/put.ts
@@ -0,0 +1,29 @@
+import { NextApiResponse } from "next"
+import { prisma } from "../../../helpers/db"
+import { PutUserBody, SessionUser } from "../../../types/types"
+
+export default async function put(
+   res: NextApiResponse,
+   body: PutUserBody,
+   user: SessionUser
+) {
+   // Update account in DB
+   try {
+      await prisma.user.update({
+         where: {
+            email: user.email,
+         },
+         data: {
+            ...body,
+         },
+      })
+      // Return creation status
+      return res.status(200).json({
+         message: "Updated user.",
+      })
+   } catch (error: any) {
+      return res.status(500).json({
+         message: "Unable to update user information. Please try again later.",
+      })
+   }
+}
